31593d929e644f269ed01fc689ea30c904cbb354
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-08-16 06:37:23 -0400

    messing with discovery

--- p2p/discovery.go
+++ p2p/discovery.go
@@ -5,51 +5,54 @@ import (
 	"fmt"
 	pstore "github.com/libp2p/go-libp2p-peerstore"
 	discovery "github.com/libp2p/go-libp2p/p2p/discovery"
+	// ma "github.com/multiformats/go-multiaddr"
 	"time"
 )
 
 func (n *QriNode) HandlePeerFound(pinfo pstore.PeerInfo) {
 	// fmt.Println("trying peer info: ", pinfo)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*20)
 	defer cancel()
 	if err := n.Host.Connect(ctx, pinfo); err != nil {
 		fmt.Println("Failed to connect to peer found by discovery: ", err)
+		return
 	}
 
-	data, _ := pinfo.MarshalJSON()
-	fmt.Println(string(data))
-
-	n.Host.Peerstore().AddAddr(pinfo.ID, pinfo.Addrs[0], time.Hour)
+	// data, _ := pinfo.MarshalJSON()
+	// fmt.Println(string(data))
 
 	// p, err := n.repo.Profile()
 	// if err != nil {
 	// 	return
 	// }
 
-	res, err := n.SendMessage(pinfo.Addrs[0].String(), &Message{
-		Type:    MtProfile,
-		Payload: nil,
-	})
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+	// peerAddr, _ := ma.NewMultiaddr(fmt.Sprintf("/ipfs/%s", pinfo.ID.Pretty()))
 
-	if res.Phase == MpResponse {
-		fmt.Println(res)
+	// addr := pinfo.Addrs[0].Encapsulate(peerAddr)
 
-		// peers, err := n.repo.Peers()
-		// if err != nil {
-		// 	return
-		// }
-	}
+	// res, err := n.SendMessage(addr.String(), &Message{
+	// 	Type:    MtProfile,
+	// 	Payload: nil,
+	// })
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+
+	// if res.Phase == MpResponse {
+	// 	fmt.Println(res)
+	// 	// peers, err := n.repo.Peers()
+	// 	// if err != nil {
+	// 	// 	return
+	// 	// }
+	// }
 
 	fmt.Println("connected to peer: ", pinfo.ID.Pretty())
 }
 
 // StartDiscovery initiates peer discovery
 func (n *QriNode) StartDiscovery() error {
-	service, err := discovery.NewMdnsService(context.Background(), n.Host, time.Second*2)
+	service, err := discovery.NewMdnsService(context.Background(), n.Host, time.Second*10)
 	if err != nil {
 		return err
 	}
