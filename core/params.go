926c7a7976f85ff9037f5856390be8fb25b54e0a
Thomas Osterbind <osterbit@onid.oregonstate.edu>
2017-11-07 11:37:22 -0500

    refactor(ListParams): Added a Clean method to ListParams
    - ListParams Clean method validates ListParams Limit and Offset fields
    - Given invalid values, it will update the calling instance's fields with valid defaults

--- core/params.go
+++ core/params.go
@@ -22,13 +22,10 @@ type ListParams struct {
 
 // ListParamsFromRequest extracts ListParams from an http.Request pointer
 func ListParamsFromRequest(r *http.Request) ListParams {
-	var lp ListParams
+	lp := &ListParams{}
 	var pageIndex int
 	if i, err := util.ReqParamInt("pageSize", r); err == nil {
 		lp.Limit = i
-	}
-	if lp.Limit <= 0 {
-		lp.Limit = DEFAULT_PAGE_SIZE
 	}
 	if i, err := util.ReqParamInt("page", r); err == nil {
 		pageIndex = i
@@ -36,10 +33,20 @@ func ListParamsFromRequest(r *http.Request) ListParams {
 	if pageIndex < 0 {
 		pageIndex = 0
 	}
+	lp.Clean()
 	lp.Offset = pageIndex * lp.Limit
 	// lp.OrderBy defaults to empty string
-	return lp
+	return *lp
 
+}
+
+func (lp *ListParams) Clean() {
+	if lp.Limit <= 0 {
+		lp.Limit = DEFAULT_PAGE_SIZE
+	}
+	if lp.Offset < 0 {
+		lp.Offset = 0
+	}
 }
 
 // Page converts a ListParams struct to a util.Page struct
