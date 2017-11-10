e8611d0a821180d06fcda304013c27df00d3406e
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-11-10 08:21:52 -0500

    fix(core.PeerRequests.List): remove current user from peers list
    added a quick check to remove the current user from the list method.
    we still need a test to avoid shipping a regression.
    
    closes #115

---
+++ core/peers_test.go
@@ -0,0 +1,40 @@
+package core
+
+import (
+	"testing"
+
+	"github.com/qri-io/qri/p2p"
+	"github.com/qri-io/qri/repo/profile"
+)
+
+func TestPeerRequestsList(t *testing.T) {
+	cases := []struct {
+		p   *ListParams
+		res []*profile.Profile
+		err string
+	}{
+		{&ListParams{}, nil, ""},
+		// {&ListParams{Data: badDataFile}, nil, "error determining dataset schema: no file extension provided"},
+		// {&ListParams{DataFilename: badDataFile.FileName(), Data: badDataFile}, nil, "error determining dataset schema: EOF"},
+		// {&ListParams{DataFilename: jobsByAutomationFile.FileName(), Data: jobsByAutomationFile}, nil, ""},
+		// TODO - need a test that confirms that this node's identity is never present in peers list
+	}
+
+	mr, _, err := NewTestRepo()
+	if err != nil {
+		t.Errorf("error allocating test repo: %s", err.Error())
+		return
+	}
+
+	// TODO - need to upgrade this to include a mock node
+	req := NewPeerRequests(mr, &p2p.QriNode{})
+	for i, c := range cases {
+		got := []*profile.Profile{}
+		err := req.List(c.p, &got)
+
+		if !(err == nil && c.err == "" || err != nil && err.Error() == c.err) {
+			t.Errorf("case %d error mismatch: expected: %s, got: %s", i, c.err, err)
+			continue
+		}
+	}
+}
