31f7d9088bfe986b87a7ff3970de3538ed11f04a
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-11-17 09:34:58 -0500

    test(repo.Namespace): added repo.Namespace tests
    added tests for repo namespace, made repo/fs implementation pass

--- repo/fs/fs_test.go
+++ repo/fs/fs_test.go
@@ -10,11 +10,16 @@ import (
 )
 
 func TestRepo(t *testing.T) {
-	r, err := NewRepo(memfs.NewMapstore(), filepath.Join(os.TempDir(), "qri_repo_test"), "test_repo_id")
+	path := filepath.Join(os.TempDir(), "qri_repo_test")
+	r, err := NewRepo(memfs.NewMapstore(), path, "test_repo_id")
 	if err != nil {
 		t.Errorf("error creating repo: %s", err.Error())
 		return
 	}
 
 	test.RunRepoTests(t, r)
+
+	if err := os.RemoveAll(path); err != nil {
+		t.Errorf("error cleaning up after test", err.Error())
+	}
 }
