ae250b6552ecea75873e8f5b0019772e93e7ba1f
Kasey <klhuizinga@gmail.com>
2018-06-04 11:14:19 -0400

    docs(cmd/remove): add usage to remove command

--- cmd/remove.go
+++ cmd/remove.go
@@ -12,9 +12,9 @@ var datasetRemoveCmd = &cobra.Command{
 	Aliases: []string{"rm", "delete"},
 	Short:   "remove a dataset from your local repository",
 	Long: `
-remove gets rid of datasets. After running remove, qri will no longer list your 
-dataset as being available locally. By default remove frees up the space taken 
-up by the dataset, but not right away. This is because the IPFS repo that’s 
+Remove gets rid of a dataset from your qri node. After running remove, qri will 
+no longer list your dataset as being available locally. By default, remove frees
+up the space taken up by the dataset, but not right away. The IPFS repo that’s 
 storing the data will need to garbage-collect that data when it’s good & ready, 
 which could be anytime. If you’re running low on space, garbage collection will 
 be sooner. 
@@ -24,6 +24,8 @@ adjust this cap using IPFS, qri will respect it.
 
 In the future we’ll add a flag that’ll force immediate removal of a dataset from
 both qri & IPFS. Promise.`,
+	Example: `  remove a dataset named annual_pop:
+  $ qri remove me/annual_pop`,
 	Annotations: map[string]string{
 		"group": "dataset",
 	},
