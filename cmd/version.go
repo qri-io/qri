d3c4e4d62fc3eae6b2a7dfaa1e93fd2fc2b6aaef
Dustin Long <me@dustmop.io>
2018-06-07 16:48:36 -0400

    refactor(schema): Replace schemaScanMode with TopLevelType.

--- cmd/version.go
+++ cmd/version.go
@@ -3,7 +3,7 @@ package cmd
 import "github.com/spf13/cobra"
 
 // VersionNumber is the current version qri
-const VersionNumber = "0.4.0"
+const VersionNumber = "0.4.1-dev"
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
