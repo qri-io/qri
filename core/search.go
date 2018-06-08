3e847ee51a32350704606262bbd3efa8fc2317c3
Thomas Osterbind <tom@qri.io>
2018-06-08 11:30:01 -0400

    chore: added core search skeleton

--- core/search.go
+++ core/search.go
@@ -1,13 +1,65 @@
 package core
 
-// import (
-// 	"fmt"
-// 	"net/rpc"
+import (
+	"fmt"
+	"net/rpc"
 
-// 	"github.com/qri-io/cafs"
-// 	"github.com/qri-io/qri/repo"
-// 	"github.com/qri-io/qri/repo/fs"
-// )
+	// 	"github.com/qri-io/cafs"
+	"github.com/qri-io/qri/repo"
+	// 	"github.com/qri-io/qri/repo/fs"
+)
+
+// SearchRequests encapsulates business logic for the qri search
+// command
+type SearchRequests struct {
+	cli  *rpc.Client
+	repo repo.Repo
+}
+
+// NewSearchRequests creates a SearchRequests pointer from either a repo
+// or an rpc.Client
+func NewSearchRequests(r repo.Repo, cli *rpc.Client) *SearchRequests {
+	if r != nil && cli != nil {
+		panic(fmt.Errorf("both repo and client supplied to NewSearchRequests"))
+	}
+	return &SearchRequests{
+		cli:  cli,
+		repo: r,
+	}
+}
+
+// CoreRequestsName implements the requests
+func (sr SearchRequests) CoreRequestsName() string { return "search" }
+
+// SearchParams defines paremeters for the search Method
+type SearchParams struct {
+	QueryString string
+	Limit       int
+	Offset      int
+}
+
+// SearchResult struct
+type SearchResult struct {
+	Type, ID string
+	Value    interface{}
+}
+
+// Search queries for items on qri related to given parameters
+func (sr *SearchRequests) Search(p *SearchParams, results *[]SearchResult) error {
+	if sr.cli != nil {
+		return sr.cli.Call("SearchRequests.Search", p, results)
+	}
+	// TODO: actual search implementation
+	// ...
+	// ...
+	// ...
+	// ...
+	fakeResult1 := &SearchResult{"Dataset", "ID1", "Value1"}
+	fakeResult2 := &SearchResult{"Dataset", "ID2", "Value2"}
+	*results = append(*results, *fakeResult1)
+	*results = append(*results, *fakeResult2)
+	return nil
+}
 
 // // SearchRequests encapsulates business logic for the qri search
 // // command
