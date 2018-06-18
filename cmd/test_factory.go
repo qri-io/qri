79446cd8c08f50f9f48ab61bc5a807cbef964140
Kasey <klhuizinga@gmail.com>
2018-06-18 17:06:46 -0400

    feat(TestFactory): new struct TestFactory that can be used as factory when building tests on individual commands

---
+++ cmd/test_factory.go
@@ -0,0 +1,119 @@
+package cmd
+
+import (
+	"net/rpc"
+
+	"github.com/qri-io/qri/config"
+	"github.com/qri-io/qri/lib"
+	"github.com/qri-io/qri/p2p"
+	"github.com/qri-io/qri/repo"
+	"github.com/qri-io/qri/repo/test"
+)
+
+// TestFactory holds the states of the test command
+type TestFactory struct {
+	IOStreams
+	// QriRepoPath is the path to the QRI repository
+	qriRepoPath string
+	// IpfsFsPath is the path to the IPFS repo
+	ipfsFsPath string
+	// NoPrompt Disables all promt messages
+	NoPrompt bool
+	// NoColor disables colorized output
+	NoColor bool
+	// path to configuration object
+	ConfigPath string
+
+	// Configuration object
+	config *config.Config
+	node   *p2p.QriNode
+	repo   repo.Repo
+	rpc    *rpc.Client
+}
+
+// NewTestFactory creates TestFactory object with an in memory test repo
+func NewTestFactory(streams IOStreams) (tf TestFactory, err error) {
+	repo, err := test.NewTestRepo(nil)
+	if err != nil {
+		return
+	}
+
+	return TestFactory{
+		IOStreams:   streams,
+		qriRepoPath: "",
+		ipfsFsPath:  "",
+		NoPrompt:    true,
+		NoColor:     true,
+		ConfigPath:  "",
+
+		repo:   repo,
+		rpc:    nil,
+		config: nil,
+		node:   nil,
+	}, nil
+}
+
+// Config returns from internal state
+func (t TestFactory) Config() (*config.Config, error) {
+	return t.config, nil
+}
+
+// IpfsFsPath returns from internal state
+func (t TestFactory) IpfsFsPath() string {
+	return t.ipfsFsPath
+}
+
+// QriRepoPath returns from internal state
+func (t TestFactory) QriRepoPath() string {
+	return t.qriRepoPath
+}
+
+// Repo returns from internal state
+func (t TestFactory) Repo() (repo.Repo, error) {
+	return t.repo, nil
+}
+
+// RPC returns from internal state
+func (t TestFactory) RPC() *rpc.Client {
+	return nil
+}
+
+// DatasetRequests generates a lib.DatasetRequests from internal state
+func (t TestFactory) DatasetRequests() (*lib.DatasetRequests, error) {
+	return lib.NewDatasetRequests(t.repo, t.rpc), nil
+}
+
+// RegistryRequests generates a lib.RegistryRequests from internal state
+func (t TestFactory) RegistryRequests() (*lib.RegistryRequests, error) {
+	return lib.NewRegistryRequests(t.repo, t.rpc), nil
+}
+
+// HistoryRequests generates a lib.HistoryRequests from internal state
+func (t TestFactory) HistoryRequests() (*lib.HistoryRequests, error) {
+	return lib.NewHistoryRequests(t.repo, t.rpc), nil
+}
+
+// PeerRequests generates a lib.PeerRequests from internal state
+func (t TestFactory) PeerRequests() (*lib.PeerRequests, error) {
+	return lib.NewPeerRequests(nil, t.rpc), nil
+}
+
+// ProfileRequests generates a lib.ProfileRequests from internal state
+func (t TestFactory) ProfileRequests() (*lib.ProfileRequests, error) {
+	return lib.NewProfileRequests(t.repo, t.rpc), nil
+}
+
+// SelectionRequests creates a lib.SelectionRequests from internal state
+func (t TestFactory) SelectionRequests() (*lib.SelectionRequests, error) {
+	return lib.NewSelectionRequests(t.repo, t.rpc), nil
+}
+
+// SearchRequests generates a lib.SearchRequests from internal state
+func (t TestFactory) SearchRequests() (*lib.SearchRequests, error) {
+	return lib.NewSearchRequests(t.repo, t.rpc), nil
+}
+
+// RenderRequests generates a lib.RenderRequests from internal state
+func (t TestFactory) RenderRequests() (*lib.RenderRequests, error) {
+	return lib.NewRenderRequests(t.repo, t.rpc), nil
+}
