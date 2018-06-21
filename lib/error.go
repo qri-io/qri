207e3141d5703e49f866bd01e8ef41028f0cb272
Kasey <klhuizinga@gmail.com>
2018-06-21 11:00:17 -0400

    feat(lib.Error): new type Error that satisfies the error interface
    lib.Error wraps and `err` error and a `msg` string.  lib.Error gives us an opportunity to couple dev focused error messages with more user-friendly errors. Before returning an error message to the user, we can check if it actually a lib.Error. If it is, we can send Error.msg to the user instead.

---
+++ lib/error.go
@@ -0,0 +1,28 @@
+package lib
+
+// Error wraps an error and satisfies the error interface.
+// It couples more developer focused errors with more
+// user-friendly errors. If a msg exists, you can send an
+// e.Message() to the user, rather than the standard error
+type Error struct {
+	err error
+	msg string
+}
+
+// Error let's the Error struct satisfy the error interface
+func (e Error) Error() string {
+	return e.err.Error()
+}
+
+// Message returns the e.msg string
+func (e Error) Message() string {
+	return e.msg
+}
+
+// NewError creates an Error from an error and string
+func NewError(err error, msg string) Error {
+	return Error{
+		err: err,
+		msg: msg,
+	}
+}
