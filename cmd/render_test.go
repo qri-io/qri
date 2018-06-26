2be68c47b3f53865f0f45ea0736aa3658d6bf596
Kasey <klhuizinga@gmail.com>
2018-06-25 22:53:44 -0400

    test(cmd/render): add tests to Complete and Validate functions for render command

---
+++ cmd/render_test.go
@@ -0,0 +1,135 @@
+package cmd
+
+import (
+	"testing"
+
+	"github.com/qri-io/qri/lib"
+)
+
+func TestRenderComplete(t *testing.T) {
+	streams, in, out, errs := NewTestIOStreams()
+	setNoColor(true)
+
+	f, err := NewTestFactory()
+	if err != nil {
+		t.Errorf("error creating new test factory: %s", err)
+		return
+	}
+
+	cases := []struct {
+		args   []string
+		expect string
+		err    string
+	}{
+		{[]string{}, "", ""},
+		{[]string{"test"}, "test", ""},
+		{[]string{"test", "test2"}, "test", ""},
+	}
+
+	for i, c := range cases {
+		opt := &RenderOptions{
+			IOStreams: streams,
+		}
+
+		opt.Complete(f, c.args)
+
+		if c.err != errs.String() {
+			t.Errorf("case %d, error mismatch. Expected: '%s', Got: '%s'", i, c.err, errs.String())
+			ioReset(in, out, errs)
+			continue
+		}
+
+		if c.expect != opt.Ref {
+			t.Errorf("case %d, opt.Ref not set correctly. Expected: '%s', Got: '%s'", i, c.expect, opt.Ref)
+			ioReset(in, out, errs)
+			continue
+		}
+
+		if opt.RenderRequests == nil {
+			t.Errorf("case %d, opt.RenderRequests not set.", i)
+			ioReset(in, out, errs)
+			continue
+		}
+		ioReset(in, out, errs)
+	}
+}
+
+func TestRenderValidate(t *testing.T) {
+	cases := []struct {
+		ref string
+		err string
+		msg string
+	}{
+		{"", ErrBadArgs.Error(), "peername and dataset name needed in order to render, for example:\n   $ qri render me/dataset_name\nsee `qri render --help` from more info"},
+		{"me/test", "", ""},
+	}
+	for i, c := range cases {
+		opt := &RenderOptions{
+			Ref: c.ref,
+		}
+
+		err := opt.Validate()
+		if (err == nil && c.err != "") || (err != nil && c.err != err.Error()) {
+			t.Errorf("case %d, mismatched error. Expected: %s, Got: %s", i, c.err, err)
+			continue
+		}
+		if libErr, ok := err.(lib.Error); ok {
+			if libErr.Message() != c.msg {
+				t.Errorf("case %d, mismatched user-friendly message. Expected: '%s', Got: '%s'", i, c.msg, libErr.Message())
+				continue
+			}
+		} else if c.msg != "" {
+			t.Errorf("case %d, mismatched user-friendly message. Expected: '%s', Got: ''", i, c.msg)
+			continue
+		}
+	}
+}
+
+// func TestRenderRun(t *testing.T) {
+// 	streams, in, out, errs := NewTestIOStreams()
+// 	setNoColor(true)
+
+// 	f, err := NewTestFactory()
+// 	if err != nil {
+// 		t.Errorf("error creating new test factory: %s", err)
+// 		return
+// 	}
+
+// 	cases := []struct{}{}
+
+// 	for i, c := range cases {
+// 		slr, err := f.RenderRequests()
+// 		if err != nil {
+// 			t.Errorf("case %d, error creating dataset request: %s", i, err)
+// 			continue
+// 		}
+
+// 		opt := &RenderOptions{}
+
+// 		err = opt.Run()
+// 		if (err == nil && c.err != "") || (err != nil && c.err != err.Error()) {
+// 			t.Errorf("case %d, mismatched error. Expected: '%s', Got: '%v'", i, c.err, err)
+// 			ioReset(in, out, errs)
+// 			continue
+// 		}
+
+// 		if libErr, ok := err.(lib.Error); ok {
+// 			if libErr.Message() != c.msg {
+// 				t.Errorf("case %d, mismatched user-friendly message. Expected: '%s', Got: '%s'", i, c.msg, libErr.Message())
+// 				ioReset(in, out, errs)
+// 				continue
+// 			}
+// 		} else if c.msg != "" {
+// 			t.Errorf("case %d, mismatched user-friendly message. Expected: '%s', Got: ''", i, c.msg)
+// 			ioReset(in, out, errs)
+// 			continue
+// 		}
+
+// 		if c.expected != out.String() {
+// 			t.Errorf("case %d, output mismatch. Expected: '%s', Got: '%s'", i, c.expected, out.String())
+// 			ioReset(in, out, errs)
+// 			continue
+// 		}
+// 		ioReset(in, out, errs)
+// 	}
+// }
