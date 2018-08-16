4bdcbce576eff90c36d27b550f8db11afc31d27e
Kasey <klhuizinga@gmail.com>
2018-08-16 11:44:16 -0400

    docs(cmd/add.go): updating docs to match functionality

--- cmd/add.go
+++ cmd/add.go
@@ -6,21 +6,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
 // TODO: Tests.
-
 
 // NewAddCommand creates an add command
 func NewAddCommand(f Factory, ioStreams IOStreams) *cobra.Command {
 	o := &AddOptions{IOStreams: ioStreams}
 	cmd := &cobra.Command{
-		Use:        "add",
-		Short:      "Add a dataset",
+		Use:   "add",
+		Short: "Add a dataset from another peer",
 		Annotations: map[string]string{
 			"group": "dataset",
 		},
 		Long: `
-Add retrieves a dataset owned by another peer and adds it to your repo.`,
+Add retrieves a dataset owned by another peer and adds it to your repo. 
+The dataset reference of the dataset will remain the same, including 
+the name of the peer that originally added the dataset.`,
 		Example: `  add a dataset named their_data, owned by other_peer:
   $ qri add other_peer/their_data`,
 		RunE: func(cmd *cobra.Command, args []string) error {
