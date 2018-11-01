ff27671d42222b0cd5107850f0087661dcaaeda3
b5 <sparkle_pony_2000@qri.io>
2018-11-01 19:06:13 -0400

    test(p2p.RequestLogDiff): add log diff p2p method test

--- p2p/log_diff_test.go
+++ p2p/log_diff_test.go
@@ -1,73 +1,71 @@
 package p2p
 
 import (
+	"context"
 	"testing"
+
+	"github.com/qri-io/cafs"
+	"github.com/qri-io/dataset/dstest"
+	"github.com/qri-io/ioes"
+	"github.com/qri-io/qri/base"
+	p2ptest "github.com/qri-io/qri/p2p/test"
 )
 
 func TestRequestLogDiff(t *testing.T) {
-	// ctx := context.Background()
-	// streams := ioes.NewDiscardIOStreams()
-	// factory := p2ptest.NewTestNodeFactory(NewTestableQriNode)
-	// testPeers, err := p2ptest.NewTestDirNetwork(ctx, factory)
-	// if err != nil {
-	// 	t.Fatalf("error creating network: %s", err.Error())
-	// }
-	// if err := p2ptest.ConnectQriNodes(ctx, testPeers); err != nil {
-	// 	t.Fatalf("error connecting peers: %s", err.Error())
-	// }
+	ctx := context.Background()
+	streams := ioes.NewDiscardIOStreams()
+	factory := p2ptest.NewTestNodeFactory(NewTestableQriNode)
+	testPeers, err := p2ptest.NewTestDirNetwork(ctx, factory)
+	if err != nil {
+		t.Fatalf("error creating network: %s", err.Error())
+	}
+	if err := p2ptest.ConnectQriNodes(ctx, testPeers); err != nil {
+		t.Fatalf("error connecting peers: %s", err.Error())
+	}
 
-	// peers := asQriNodes(testPeers)
+	peers := asQriNodes(testPeers)
 
-	// tc, err := dstest.NewTestCaseFromDir("testdata/tim/craigslist")
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
+	tc, err := dstest.NewTestCaseFromDir("testdata/tim/craigslist")
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	// // add a dataset to tim
-	// ref, _, err := base.CreateDataset(peers[4].Repo, streams, tc.Name, tc.Input, tc.BodyFile(), false, true)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
+	// add a dataset to peer 5
+	ref, _, err := base.CreateDataset(peers[4].Repo, streams, tc.Name, tc.Input, tc.BodyFile(), false, true)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	// //
-	// // peers[]
+	// simulate IPFS connection (we're not on IPFS when running tests)
+	peers[3].Repo.Store().(*cafs.MapStore).AddConnection(peers[4].Repo.Store().(*cafs.MapStore))
 
-	// prevTitle := tc.Input.Meta.Title
-	// tc.Input.Meta.Title = "update"
-	// tc.Input.PreviousPath = ref.Path
-	// defer func() {
-	// 	// because test cases are cached for performance, we need to clean up any mutation to
-	// 	// testcase input
-	// 	tc.Input.Meta.Title = prevTitle
-	// 	tc.Input.PreviousPath = ref.Path
-	// }()
+	// give that dataset to peer 4
+	if err := base.FetchDataset(peers[3].Repo, &ref, true, false); err != nil {
+		t.Fatalf("error fetching dataset: %s", err)
+	}
 
-	// ref2, _, err := base.CreateDataset(peers[4].Repo, streams, tc.Name, tc.Input, tc.BodyFile(), false, true)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
+	prevTitle := tc.Input.Meta.Title
+	tc.Input.Meta.Title = "update"
+	tc.Input.PreviousPath = ref.Path
+	defer func() {
+		// because test cases are cached for performance, we need to clean up any mutation to
+		// testcase input
+		tc.Input.Meta.Title = prevTitle
+		tc.Input.PreviousPath = ""
+	}()
 
-	// t.Logf("testing RequestDatasetLog message with %d peers", len(peers))
-	// // var wg sync.WaitGroup
-	// for i, p1 := range peers {
-	// 	for _, p2 := range peers[i+1:] {
-	// 		// TODO - having these in parallel is causing races when encoding logs
-	// 		// wg.Add(1)
-	// 		// go func(p1, p2 *QriNode) {
-	// 		// 	defer wg.Done()
+	// add an update on peer 5
+	ref2, _, err := base.CreateDataset(peers[4].Repo, streams, tc.Name, tc.Input, tc.BodyFile(), false, true)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	// 		refs, err := p1.RequestDatasetLog(ref, 100, 0)
-	// 		if err != nil {
-	// 			t.Errorf("%s -> %s error: %s", p1.ID.Pretty(), p2.ID.Pretty(), err.Error())
-	// 		}
-	// 		if refs == nil {
-	// 			t.Error("profile shouldn't be nil")
-	// 			return
-	// 		}
-	// 		// t.Log(refs)
-	// 		// }(p1, p2)
-	// 	}
-	// }
+	ldr, err := peers[3].RequestLogDiff(&ref)
+	if err != nil {
+		t.Error(err)
+	}
 
-	// wg.Wait()
+	if !ldr.Head.Equal(ref2) {
+		t.Errorf("wrong head sent")
+	}
 }
