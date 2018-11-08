00ca81c7af97bad35b511dda399776a30d376f76
b5 <sparkle_pony_2000@qri.io>
2018-11-08 08:15:10 -0500

    feat(rev): add rev package, add LoadRevs to base
    Qri needs new tools for pulling components out of history, and ways of
    specifying those components. This adds a version of revision parsing that's inspired by git, but matched to our needs

---
+++ base/revisions.go
@@ -0,0 +1,91 @@
+package base
+
+import (
+	datastore "github.com/ipfs/go-datastore"
+	"github.com/qri-io/dataset"
+	"github.com/qri-io/dataset/dsfs"
+	"github.com/qri-io/qri/repo"
+	"github.com/qri-io/qri/rev"
+)
+
+// LoadRevs grabs a component of a dataset that exists <n>th generation ancestor
+// of the referenced version, where presence of a component in a previous snapshot constitutes ancestry
+func LoadRevs(r repo.Repo, ref repo.DatasetRef, revs []*rev.Rev) (res *dataset.Dataset, err error) {
+	var ds *dataset.Dataset
+	res = &dataset.Dataset{}
+	for {
+		if ds, err = dsfs.LoadDataset(r.Store(), datastore.NewKey(ref.Path)); err != nil {
+			return
+		}
+
+		done := true
+		for _, rev := range revs {
+			if !sel(rev, ds, res) && done {
+				done = false
+			}
+		}
+		if done {
+			break
+		}
+
+		ref.Path = ds.PreviousPath
+		if ref.Path == "" {
+			break
+		}
+	}
+	return res, nil
+}
+
+func sel(r *rev.Rev, ds, res *dataset.Dataset) bool {
+	switch r.Field {
+	case "ds":
+		r.Gen--
+		if r.Gen == 0 {
+			res = ds
+		}
+	case "bd":
+		if ds.BodyPath != "" {
+			r.Gen--
+			if r.Gen == 0 {
+				res.BodyPath = ds.BodyPath
+			}
+		}
+	case "md":
+		if ds.Meta != nil {
+			r.Gen--
+			if r.Gen == 0 {
+				res.Meta = ds.Meta
+			}
+		}
+	case "tf":
+		if ds.Transform != nil {
+			r.Gen--
+			if r.Gen == 0 {
+				res.Transform = ds.Transform
+			}
+		}
+	case "cm":
+		if ds.Commit != nil {
+			r.Gen--
+			if r.Gen == 0 {
+				res.Commit = ds.Commit
+			}
+		}
+	case "vz":
+		if ds.Viz != nil {
+			r.Gen--
+			if r.Gen == 0 {
+				res.Viz = ds.Viz
+			}
+		}
+	case "st":
+		if ds.Structure != nil {
+			r.Gen--
+			if r.Gen == 0 {
+				res.Structure = ds.Structure
+			}
+		}
+	}
+
+	return r.Gen == 0
+}
---
+++ base/revisions_test.go
@@ -0,0 +1,55 @@
+package base
+
+import (
+	"testing"
+
+	datastore "github.com/ipfs/go-datastore"
+	"github.com/qri-io/dataset"
+	"github.com/qri-io/dataset/dsfs"
+	"github.com/qri-io/qri/repo"
+	"github.com/qri-io/qri/rev"
+)
+
+func TestLoadRevisions(t *testing.T) {
+	r := newTestRepo(t)
+	ref := addCitiesDataset(t, r)
+
+	cities, err := dsfs.LoadDataset(r.Store(), datastore.NewKey(ref.Path))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cases := []struct {
+		ref  repo.DatasetRef
+		revs string
+		ds   *dataset.Dataset
+		err  string
+	}{
+		// TODO - both of these are failing, let's make 'em work:
+		// "ds" Qri value is mismatching
+		// {ref, "ds", cities, ""},
+		// Logic on what to do in "body" is a little confusing atm, do we set BodyPath
+		// and move on?
+		// {ref, "bd", cities, ""},
+
+		{ref, "tf", &dataset.Dataset{Transform: cities.Transform}, ""},
+		{ref, "md,vz,tf,st", &dataset.Dataset{Transform: cities.Transform, Meta: cities.Meta, Structure: cities.Structure}, ""},
+	}
+
+	for i, c := range cases {
+		revs, err := rev.ParseRevs(c.revs)
+		if err != nil {
+			t.Errorf("case %d error parsing revs: %s", i, err)
+			continue
+		}
+
+		got, err := LoadRevs(r, c.ref, revs)
+		if !(err == nil && c.err == "" || err != nil && err.Error() == c.err) {
+			t.Errorf("case %d error mismatch. expected: %s, got: %s", i, c.err, err)
+		}
+
+		if err := dataset.CompareDatasets(c.ds, got); err != nil {
+			t.Errorf("case %d result mismatch: %s", i, err)
+		}
+	}
+}
