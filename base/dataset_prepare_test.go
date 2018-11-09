5d46a4ebeacbc7ea646d2cb8576e782316f67bcf
b5 <sparkle_pony_2000@qri.io>
2018-11-09 08:49:36 -0500

    test(base.Prepare): add basic tests for dataset prep funcs

--- base/dataset_prepare_test.go
+++ base/dataset_prepare_test.go
@@ -1,21 +1,49 @@
 package base
 
-// func TestPrepareDatasetSave(t *testing.T) {
-// 	r := newTestRepo(t)
-// 	ref := addCitiesDataset(t, r)
+import (
+	"testing"
 
-// 	tc, err := dstest.NewTestCaseFromDir(testdataPath("cities"))
-// 	if err != nil {
-// 		t.Fatal(err.Error())
-// 	}
+	"github.com/qri-io/cafs"
+	"github.com/qri-io/dataset"
+)
 
-// 	dsp := tc.Input.Encode()
-// 	dsp.Meta.Title = "updated"
-// 	dsp.Name = ref.Name
-// 	dsp.Peername = ref.Peername
+func TestPrepareDatasetSave(t *testing.T) {
+	r := newTestRepo(t)
+	ref := addCitiesDataset(t, r)
 
-// 	_, _, _, err = PrepareDatasetSave(r, dsp)
-// 	if err != nil {
-// 		t.Error(err.Error())
-// 	}
-// }
+	_, _, _, err := PrepareDatasetSave(r, ref.Peername, ref.Name)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	_, _, _, err = PrepareDatasetSave(r, "me", "non-existent")
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func TestInferValues(t *testing.T) {
+	r := newTestRepo(t)
+	pro, err := r.Profile()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	name := ""
+	body := cafs.NewMemfileBytes("gabba gabba hey.csv", []byte("a,b,c,c,s,v"))
+	ds := &dataset.Dataset{}
+	if _, err = InferValues(pro, &name, ds, body); err != nil {
+		t.Error(err)
+	}
+	// TODO - lol fix varname, so broken
+	expectName := "gabba_gabba_heycsv"
+	if expectName != name {
+		t.Errorf("inferred name mismatch. expected: '%s', got: '%s'", expectName, name)
+	}
+}
+
+func TestValidateDataset(t *testing.T) {
+	if err := ValidateDataset("this name has spaces", nil); err == nil {
+		t.Errorf("expected invalid name to fail")
+	}
+}
