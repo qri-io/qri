3436c98bd84c31b3da668fd81db9698ab18cdad9
Kasey <klhuizinga@gmail.com>
2018-11-28 17:54:18 -0500

    refactor(lib): remove `RegistryRequests.Status` add `RegistryRequests.Get`
    Again, we want Get to fill the spot of status, except we want Get to return the dataset.

--- lib/registry_test.go
+++ lib/registry_test.go
@@ -2,11 +2,23 @@ package lib
 
 import (
 	"testing"
+	"time"
 
+	"github.com/qri-io/dataset/dsfs"
+	"github.com/qri-io/qri/repo"
 	"github.com/qri-io/registry/regserver/mock"
 )
 
 func TestRegistryRequests(t *testing.T) {
+	// we need to artificially specify the timestamp
+	// we use the dsfs.Timestamp func variable to override
+	// the actual time
+	prevTs := dsfs.Timestamp
+	dsfs.Timestamp = func() time.Time { return time.Date(2001, 01, 01, 01, 01, 01, 01, time.UTC) }
+	defer func() {
+		dsfs.Timestamp = prevTs
+	}()
+
 	reg := mock.NewMemRegistry()
 	cli, _ := mock.NewMockServerRegistry(reg)
 	node := newTestQriNodeRegClient(t, cli)
@@ -20,6 +32,26 @@ func TestRegistryRequests(t *testing.T) {
 	done := false
 	if err := req.Publish(params, &done); err != nil {
 		t.Fatal(err)
+	}
+
+	// test getting a dataset from the registry
+	citiesRef := repo.DatasetRef{
+		Peername: "me",
+		Name:     "cities",
+	}
+	citiesRes := repo.DatasetRef{}
+	if err := req.Get(&citiesRef, &citiesRes); err != nil {
+		t.Error(err)
+	}
+
+	if citiesRes.Path != "/map/QmW3QAZWmLcjS1RyPgYDa59w23k5VeyPHim2b8Zj7z8Zpo" {
+		t.Errorf("error getting dataset from registry, expected path to be '/map/QmW3QAZWmLcjS1RyPgYDa59w23k5VeyPHim2b8Zj7z8Zpo', got %s", citiesRes.Path)
+	}
+	if citiesRes.Dataset == nil {
+		t.Errorf("error getting dataset from registry, dataset is nil")
+	}
+	if citiesRes.Published != true {
+		t.Errorf("error getting dataset from registry, expected published to be 'true'")
 	}
 
 	params = &PublishParams{Ref: ref2, Pin: true}
