e00d5169a60fbd42729deb00b0d5508b58987996
Dustin Long <me@dustmop.io>
2019-01-14 15:21:52 -0500

    fix(list): Listing a non-existant profile should not crash
    Before, running `qri list not_found` would try to deref a nil error value,
    leading to a crash. Instead, check if the returned profile pointer is
    null and print an appropriate error.

--- actions/list_datasets_test.go
+++ actions/list_datasets_test.go
@@ -19,3 +19,17 @@ func TestListDatasets(t *testing.T) {
 		t.Error("expected one dataset response")
 	}
 }
+
+func TestListDatasetsNotFound(t *testing.T) {
+	node := newTestNode(t)
+	addCitiesDataset(t, node)
+
+	_, err := ListDatasets(node, &repo.DatasetRef{Peername: "not_found"}, 1, 0, false, false)
+	if err == nil {
+		t.Error("expected to get error")
+	}
+	expect := "profile not found: \"not_found\""
+	if expect != err.Error() {
+		t.Errorf("expected error \"%s\", got \"%s\"", expect, err.Error())
+	}
+}
