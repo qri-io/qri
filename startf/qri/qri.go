90430f74d91a7f8f3e12fc4af4a7289b1aa68e60
b5 <sparkle_pony_2000@qri.io>
2019-05-31 10:52:35 -0400

    chore(Makefile): add missing startf deps, add specificity to gitignore

---
+++ startf/qri/qri.go
@@ -0,0 +1,61 @@
+package qri
+
+import (
+	"fmt"
+
+	"github.com/qri-io/dataset"
+	"github.com/qri-io/qri/p2p"
+	"go.starlark.net/starlark"
+	"go.starlark.net/starlarkstruct"
+)
+
+// ModuleName defines the expected name for this module when used
+// in starlark's load() function, eg: load('qri.star', 'qri')
+const ModuleName = "qri.star"
+
+// NewModule creates a new qri module instance
+func NewModule(node *p2p.QriNode) *Module {
+	return &Module{node: node}
+}
+
+// Module encapsulates state for a qri starlark module
+type Module struct {
+	node *p2p.QriNode
+	ds   *dataset.Dataset
+}
+
+// Namespace produces this module's exported namespace
+func (m *Module) Namespace() starlark.StringDict {
+	return starlark.StringDict{
+		"qri": m.Struct(),
+	}
+}
+
+// Struct returns this module's methods as a starlark Struct
+func (m *Module) Struct() *starlarkstruct.Struct {
+	return starlarkstruct.FromStringDict(starlarkstruct.Default, m.AddAllMethods(starlark.StringDict{}))
+}
+
+// AddAllMethods augments a starlark.StringDict with all qri builtins. Should really only be used during "transform" step
+func (m *Module) AddAllMethods(sd starlark.StringDict) starlark.StringDict {
+	sd["list_datasets"] = starlark.NewBuiltin("list_datasets", m.ListDatasets)
+	return sd
+}
+
+// ListDatasets shows current local datasets
+func (m *Module) ListDatasets(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	if m.node == nil {
+		return starlark.None, fmt.Errorf("no qri node available to list datasets")
+	}
+
+	refs, err := m.node.Repo.References(1000, 0)
+	if err != nil {
+		return starlark.None, fmt.Errorf("error getting dataset list: %s", err.Error())
+	}
+
+	l := &starlark.List{}
+	for _, ref := range refs {
+		l.Append(starlark.String(ref.String()))
+	}
+	return l, nil
+}
---
+++ startf/qri/qri_test.go
@@ -0,0 +1,39 @@
+package qri
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/qri-io/dataset"
+	"go.starlark.net/starlark"
+)
+
+func TestNewModule(t *testing.T) {
+	ds := &dataset.Dataset{
+		Transform: &dataset.Transform{
+			Syntax: "starlark",
+			Config: map[string]interface{}{
+				"foo": "bar",
+			},
+		},
+	}
+
+	thread := &starlark.Thread{Load: newLoader(ds)}
+
+	// Execute test file
+	_, err := starlark.ExecFile(thread, "testdata/test.star", nil, nil)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+// load implements the 'load' operation as used in the evaluator tests.
+func newLoader(ds *dataset.Dataset) func(thread *starlark.Thread, module string) (starlark.StringDict, error) {
+	return func(thread *starlark.Thread, module string) (starlark.StringDict, error) {
+		if module == ModuleName {
+			return starlark.StringDict{"qri": NewModule(nil).Struct()}, nil
+		}
+
+		return nil, fmt.Errorf("invalid module")
+	}
+}
