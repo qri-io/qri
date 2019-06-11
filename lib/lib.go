36a4b9aa38c590646d59aea2c3c7aa7dbfd78ddc
Kasey <klhuizinga@gmail.com>
2019-06-11 15:12:29 -0400

    chore(dev version bump): bump to 0.8.2-dev, and add darwin/amd64 build to makefile

--- lib/lib.go
+++ lib/lib.go
@@ -44,7 +44,7 @@ var (
 )
 
 // VersionNumber is the current version qri
-const VersionNumber = "0.8.1"
+const VersionNumber = "0.8.2-dev"
 
 func init() {
 	// Fields like dataset.Structure.Schema contain data of arbitrary types,
