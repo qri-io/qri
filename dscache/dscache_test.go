e69c71d1e03b7a9a22f6c9312ab7ee7c12c66b51
Dustin Long <me@dustmop.io>
2020-02-07 18:21:06 -0500

    test(dscache): Align fields of dscache and VersionInfo. Test serialization.

--- dscache/dscache_test.go
+++ dscache/dscache_test.go
@@ -1,10 +1,18 @@
 package dscache
 
 import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 
+	"github.com/qri-io/qfs/localfs"
+	testPeers "github.com/qri-io/qri/config/test"
+	"github.com/qri-io/qri/dsref"
 	"github.com/qri-io/qri/logbook"
+	"github.com/qri-io/qri/repo/profile"
 )
 
 // TODO(dlong): Test NewDscache, IsEmpty, Assign, ListRefs, Update
@@ -29,5 +37,39 @@ func TestNilCallable(t *testing.T) {
 	}
 	if err = cache.Update(&logbook.Action{}); err != ErrNoDscache {
 		t.Errorf("expected '%s': got '%s'", ErrNoDscache, err)
+	}
+}
+
+func TestDscacheAssignSaveAndLoad(t *testing.T) {
+	tmpdir, err := ioutil.TempDir("", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(tmpdir)
+
+	ctx := context.Background()
+	fs := localfs.NewFS()
+
+	peerInfo := testPeers.GetTestPeerInfo(0)
+
+	// Construct a dscache, will not save without a filename
+	builder := NewBuilder()
+	builder.AddUser("test_user", profile.ID(peerInfo.PeerID).String())
+	builder.AddDsVersionInfo("abcd1", dsref.VersionInfo{})
+	builder.AddDsVersionInfo("efgh2", dsref.VersionInfo{})
+	constructed := builder.Build()
+
+	// A dscache that will save when it is assigned
+	dscacheFile := filepath.Join(tmpdir, "dscache.qfb")
+	saveable := NewDscache(ctx, fs, dscacheFile)
+	saveable.Assign(constructed)
+
+	// Load the dscache from its serialized file, verify it has correct data
+	loadable := NewDscache(ctx, fs, dscacheFile)
+	if loadable.Root.UsersLength() != 1 {
+		t.Errorf("expected, 1 user, got %d users", loadable.Root.UsersLength())
+	}
+	if loadable.Root.RefsLength() != 2 {
+		t.Errorf("expected, 2 refs, got %d refs", loadable.Root.RefsLength())
 	}
 }
