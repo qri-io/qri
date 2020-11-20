2128a0cd7ee340a5f341e6ff7d454f59e419bf52
b5 <sparkle_pony_2000@qri.io>
2020-11-20 10:31:15 -0500

    feat(stats): overhaul stats service interface, implement os stats cache

--- stats/cache.go
+++ stats/cache.go
@@ -3,10 +3,15 @@ package stats
 import (
 	"context"
 	"encoding/base32"
+	"encoding/json"
+	"errors"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sync"
+
+	"github.com/qri-io/dataset"
 )
 
 var (
@@ -14,55 +19,26 @@ var (
 	ErrCacheMiss = fmt.Errorf("stats: cache miss")
 	// ErrNoCache indicates there is no cache
 	ErrNoCache = fmt.Errorf("stats: no cache")
+	// ErrCacheCorrupt indicates a faulty stats cache
+	ErrCacheCorrupt = fmt.Errorf("stats: cache is corrupt")
 )
 
-// Cache is a store of JSON-formated stats data, keyed by path
+// Cache is a store of stats components
 // Consumers of a cache must not rely on the cache for persistence
 // Implementations are expected to maintain their own size bounding
 // semantics internally
 // Cache implementations must be safe for concurrent use, and must be
 // nil-callable
 type Cache interface {
-	// Put places stats in the cache, keyed by path
-	PutJSON(ctx context.Context, path string, r io.Reader) error
-	// JSON gets cached byte data for a path
-	JSON(ctx context.Context, path string) (r io.Reader, err error)
+	// placing a stats object in the Cache will expire all caches with a lower
+	// modTime, use a modTime of zero when no modTime is known
+	PutStats(ctx context.Context, key string, modTime int, sa *dataset.Stats) error
+	// get the cached modTime for
+	// will return ErrCacheMiss if the key does not exist
+	ModTime(ctx context.Context, key string) (modTime int, err error)
+	// Get the stats component for a given key
+	Stats(ctx context.Context, key string) (sa *dataset.Stats, err error)
 }
-
-// osCache is a stats cache stored in a directory on the local operating system
-type osCache struct {
-	root    string
-	maxSize uint64
-}
-
-var _ Cache = (*osCache)(nil)
-
-// NewOSCache creates a cache in a local direcory
-func NewOSCache(rootDir string, maxSize uint64) Cache {
-	if err := os.MkdirAll(rootDir, os.ModePerm); err != nil {
-		// log.Errorf("stat: %s", args ...interface{})
-	}
-	return osCache{
-		root:    rootDir,
-		maxSize: maxSize,
-	}
-}
-
-// Put places stats in the cache, keyed by path
-func (c osCache) PutJSON(ctx context.Context, path string, r io.Reader) error {
-	filename := fmt.Sprintf("%s.json", b32Enc.EncodeToString([]byte(path)))
-	// TODO (b5) - use this
-	_ = filepath.Join(c.root, filename)
-
-	return fmt.Errorf("not finished")
-}
-
-// JSON gets cached byte data for a path
-func (c osCache) JSON(ctx context.Context, path string) (r io.Reader, err error) {
-	return nil, ErrCacheMiss
-}
-
-var b32Enc = base32.NewEncoding("abcdefghijklmnopqrstuvwxyz234567").WithPadding(base32.NoPadding)
 
 // nilCache is a stand in for not having a cache
 // it only ever returns ErrNoCache
@@ -71,11 +47,183 @@ type nilCache bool
 var _ Cache = (*nilCache)(nil)
 
 // PutJSON places stats in the cache, keyed by path
-func (nilCache) PutJSON(ctx context.Context, path string, r io.Reader) error {
+func (nilCache) PutStats(ctx context.Context, key string, modTime int, sa *dataset.Stats) error {
 	return ErrNoCache
 }
 
+// ModTime always returns ErrCacheMiss
+func (nilCache) ModTime(ctx context.Context, key string) (modTime int, err error) {
+	return -1, ErrCacheMiss
+}
+
 // JSON gets cached byte data for a path
-func (nilCache) JSON(ctx context.Context, path string) (r io.Reader, err error) {
-	return nil, ErrNoCache
+func (nilCache) Stats(ctx context.Context, key string) (sa *dataset.Stats, err error) {
+	return nil, ErrCacheMiss
+}
+
+// osCache is a stats cache stored in a directory on the local operating system
+type osCache struct {
+	root    string
+	maxSize uint64
+
+	info   *cacheInfo
+	infoLk sync.Mutex
+}
+
+var _ Cache = (*osCache)(nil)
+
+// NewOSCache creates a cache in a local direcory
+func NewOSCache(rootDir string, maxSize uint64) (Cache, error) {
+	c := &osCache{
+		root:    rootDir,
+		maxSize: maxSize,
+	}
+
+	err := c.readCacheInfo()
+	if errors.Is(err, ErrCacheCorrupt) {
+		log.Warn("your cache of stats data is corrupt, removing all cached data")
+		err = os.RemoveAll(rootDir)
+		return c, err
+	}
+
+	if err := os.MkdirAll(rootDir, os.ModePerm); err != nil {
+		return nil, err
+	}
+
+	return c, err
+}
+
+// Put places stats in the cache, keyed by path
+func (c *osCache) PutStats(ctx context.Context, key string, modTime int, sa *dataset.Stats) error {
+	if modTime < 0 {
+		modTime = 0
+	}
+
+	key = c.cacheKey(key)
+	filename := c.componentFilepath(key)
+	data, err := json.Marshal(sa)
+	if err != nil {
+		return err
+	}
+
+	if uint64(len(data)) > c.maxSize {
+		return fmt.Errorf("stats component size exceeds maximum size of cache")
+	}
+
+	if err := ioutil.WriteFile(filename, data, 0644); err != nil {
+		return err
+	}
+
+	c.addAndPurgeExcess(key, modTime, uint64(len(data)))
+
+	return c.writeCacheInfo()
+}
+
+func (c *osCache) ModTime(ctx context.Context, key string) (modTime int, err error) {
+	var exists bool
+	if modTime, exists = c.info.ModTimes[c.cacheKey(key)]; !exists {
+		return -1, ErrCacheMiss
+	}
+
+	return modTime, nil
+}
+
+// Stats gets cached byte data for a path
+func (c *osCache) Stats(ctx context.Context, key string) (sa *dataset.Stats, err error) {
+	return nil, ErrCacheMiss
+}
+
+var b32Enc = base32.NewEncoding("abcdefghijklmnopqrstuvwxyz234567").WithPadding(base32.NoPadding)
+
+func (c *osCache) componentFilepath(cacheKey string) string {
+	return filepath.Join(c.root, fmt.Sprintf("%s.json", cacheKey))
+}
+
+func (c *osCache) cacheKey(key string) string {
+	return b32Enc.EncodeToString([]byte(key))
+}
+
+const uintSize = 32 << (^uint(0) >> 32 & 1)
+
+func (c *osCache) addAndPurgeExcess(cacheKey string, modTime int, size uint64) {
+	c.infoLk.Lock()
+	defer c.infoLk.Unlock()
+	c.info.Sizes[cacheKey] = uint64(len(data))
+	c.info.ModTimes[cacheKey] = modTime
+
+	var (
+		lowestKey     string
+		lowestModTime int
+	)
+
+	for c.info.Size() > c.maxSize {
+		lowestKey = ""
+		lowestModTime = 1<<(uintSize-1) - 1
+
+		for key, modTime := range c.info.ModTimes {
+			if modTime < lowestModTime {
+				lowestKey = key
+			}
+		}
+		if lowestKey == "" {
+			break
+		}
+		if err := os.Remove(c.componentFilepath(lowestKey)); err != nil {
+			break
+		}
+		delete(c.info.Sizes, lowestKey)
+		delete(c.info.ModTimes, lowestKey)
+	}
+}
+
+const osCacheInfoFilename = "info.json"
+
+type cacheInfo struct {
+	Sizes    map[string]uint64
+	ModTimes map[string]int
+}
+
+func (ci cacheInfo) Size() (size uint64) {
+	for _, s := range ci.Sizes {
+		size += s
+	}
+	return size
+}
+
+func (c *osCache) readCacheInfo() error {
+	c.infoLk.Lock()
+	defer c.infoLk.Unlock()
+
+	name := filepath.Join(c.root, osCacheInfoFilename)
+	f, err := os.Open(name)
+	if err != nil {
+		if os.IsNotExist(err) {
+			c.info = &cacheInfo{}
+			return nil
+		}
+		return err
+	}
+
+	defer f.Close()
+
+	c.info = &cacheInfo{}
+	if err := json.NewDecoder(f).Decode(c.info); err != nil {
+		// corrupt cache
+		return fmt.Errorf("%w decoding stats info: %s", ErrCacheCorrupt, err)
+	}
+
+	return nil
+}
+
+func (c *osCache) writeCacheInfo() error {
+	c.infoLk.Lock()
+	defer c.infoLk.Unlock()
+
+	name := filepath.Join(c.root, osCacheInfoFilename)
+	data, err := json.Marshal(c.info)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(name, data, 0644)
 }
--- stats/stats.go
+++ stats/stats.go
@@ -1,4 +1,5 @@
-// Package stats calculates statistical metadata for a given dataset
+// Package stats defines a stats provider service, wrapping a cache & stats
+// component calculation
 package stats
 
 import (
@@ -6,63 +7,68 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io"
-	"sort"
+	"os"
 
-	"github.com/axiomhq/hyperloglog"
-	topk "github.com/dgryski/go-topk"
 	logger "github.com/ipfs/go-log"
 	"github.com/qri-io/dataset"
 	"github.com/qri-io/dataset/dsio"
-	"github.com/qri-io/qri/stats/histosketch"
+	"github.com/qri-io/dataset/dsstats"
+	"github.com/qri-io/qri/dsref"
 )
 
-var (
-	// StopFreqCountThreshold is the number of unique values past which we will
-	// stop keeping frequencies. This is a simplistic line of defense against
-	// unweildly memory consumption
-	StopFreqCountThreshold = 10000
-	// HistogramCentroidCount is the max number of centroids/bins for our histogram
-	// calculations more bins give better precision at the tradeoff of more CPU
-	// and memory usage. 32 is a pretty decent count for fairly large datasets.
-	HistogramCentroidCount uint = 32
+var log = logger.Logger("stats")
 
-	// package logger
-	log = logger.Logger("stats")
-)
-
-// Stats can generate an array of statistical info for a dataset
-type Stats struct {
-	cache Cache
+// Service can generate an array of statistical info for a dataset
+type Service struct {
+	cache  Cache
+	loader dsref.Loader
 }
 
 // New allocates a Stats service
-func New(cache Cache) *Stats {
+func New(cache Cache, loader dsref.Loader) *Service {
 	if cache == nil {
-		return &Stats{
-			cache: nilCache(false),
-		}
+		cache = nilCache(false)
 	}
-	return &Stats{
-		cache: cache,
+
+	return &Service{
+		cache:  cache,
+		loader: loader,
 	}
 }
 
 // JSON gets stats data as reader of JSON-formatted bytes
-func (s *Stats) JSON(ctx context.Context, ds *dataset.Dataset) (r io.Reader, err error) {
-	// check cache if there is a Path
-	// TODO (ramfox): when we are calculating stats on fsi linked
-	// datasets, we need a different metric other the `dataset.Path` to
-	// identify and store stats, since FSI datasets will not have
-	// a `dataset.Path`. This metric should perhaps come out of the
-	// `dataset.BodyFile()` since we must have a bodyFile in order to
-	// calculate the stats
-	if ds.Path != "" {
-		if r, err := s.cache.JSON(ctx, ds.Path); err == nil {
-			return r, nil
-		}
+func (s *Service) Stats(ctx context.Context, ref dsref.Ref, loadSource string) (*dataset.Stats, error) {
+	if r, err := s.cache.JSON(ctx, ref.Path); err == nil {
+		// return r, nil
 	}
 
+	ds, err := s.loader.LoadDataset(ctx, ref, loadSource)
+	if err != nil {
+		return nil, err
+	}
+
+	if ds.Stats != nil {
+		return ds.Stats, nil
+	}
+
+	return s.calcStats(ctx, ref.Path, ds)
+}
+
+func (s *Service) DatasetStats(ctx context.Context, ds *dataset.Dataset) (*dataset.Stats, error) {
+	return s.JSONWithCacheKey(ctx, ds.Path, ds)
+}
+
+// JSONWithCacheKey gets stats data as a reader of json-formatted bytes,
+func (s *Service) StatsWithCacheKey(ctx context.Context, key string, ds *dataset.Dataset) (*dataset.Stats, error) {
+	if key != "" {
+		if r, err := s.cache.JSON(ctx, key); err == nil {
+			// return r, nil
+		}
+	}
+	return s.calcStats(ctx, key, ds)
+}
+
+func (s *Service) calcStats(ctx context.Context, cacheKey string, ds *dataset.Dataset) (*dataset.Stats, error) {
 	body := ds.BodyFile()
 	if body == nil {
 		return nil, fmt.Errorf("stats: dataset has no body file")
@@ -76,497 +82,37 @@ func (s *Stats) JSON(ctx context.Context, ds *dataset.Dataset) (r io.Reader, err
 		return nil, err
 	}
 
-	acc := NewAccumulator(rdr)
-	for {
-		if _, err := acc.ReadEntry(); err != nil {
-			if err.Error() == "EOF" {
-				break
-			}
-			return nil, err
-		}
-	}
-	acc.Close()
+	acc := dsstats.NewAccumulator(ds.Structure)
 
-	data, err := json.Marshal(ToMap(acc))
+	err = dsio.EachEntry(rdr, func(i int, ent dsio.Entry, e error) error {
+		return acc.WriteEntry(ent)
+	})
 	if err != nil {
 		return nil, err
 	}
 
-	if ds.Path != "" {
+	if err = acc.Close(); err != nil {
+		return nil, err
+	}
+
+	values := dsstats.ToMap(acc)
+
+	if cacheKey != "" {
 		go func() {
-			if err := s.cache.PutJSON(context.Background(), ds.Path, bytes.NewReader(data)); err != nil {
+			data, err := json.Marshal(values)
+			if err != nil {
+				return
+			}
+			if err = s.cache.PutJSON(context.Background(), cacheKey, bytes.NewReader(data)); err != nil {
 				log.Debugf("putting stats in cache: %v", err.Error())
 			}
 		}()
 	}
 
-	return bytes.NewReader(data), nil
+	return &dataset.Stats{Stats: values}, nil
 }
 
-// Statser produces a slice of Stat objects
-type Statser interface {
-	Stats() []Stat
-}
-
-// ToMap converts stats to a Plain Old Data object
-func ToMap(s Statser) []map[string]interface{} {
-	stats := s.Stats()
-	if stats == nil {
-		return nil
-	}
-
-	sm := make([]map[string]interface{}, len(stats))
-	for i, stat := range stats {
-		sm[i] = stat.Map()
-		sm[i]["type"] = stat.Type()
-	}
-	return sm
-}
-
-// Stat describes common features of all statistical types
-type Stat interface {
-	// Type returns a string identifier for the kind of statistic being reported
-	Type() string
-	// Map reports statistical details as a map, map must not return nil
-	Map() map[string]interface{}
-}
-
-// Accumulator wraps a dsio.EntryReader, on each call to read stats
-// will update it's internal statistics
-// Consumers can only assume the return value of Accumulator.Stats is final
-// after a call to Close
-type Accumulator struct {
-	r     dsio.EntryReader
-	stats accumulator
-}
-
-var (
-	// compile time assertions that Accumulator is an EntryReader & Statser
-	_ dsio.EntryReader = (*Accumulator)(nil)
-	_ Statser          = (*Accumulator)(nil)
-)
-
-// NewAccumulator wraps an entry reader to create a stat accumulator
-func NewAccumulator(r dsio.EntryReader) *Accumulator {
-	return &Accumulator{r: r}
-}
-
-// Stats gets the statistics created by the accumulator
-func (r *Accumulator) Stats() []Stat {
-	if r.stats == nil {
-		return nil
-	}
-	if stats, ok := r.stats.(Statser); ok {
-		return stats.Stats()
-	}
-	return []Stat{r.stats}
-}
-
-// Structure gives the structure being read
-func (r *Accumulator) Structure() *dataset.Structure {
-	return r.r.Structure()
-}
-
-// ReadEntry reads one row of structured data from the reader
-func (r *Accumulator) ReadEntry() (dsio.Entry, error) {
-	ent, err := r.r.ReadEntry()
-	if err != nil {
-		return ent, err
-	}
-	if r.stats == nil {
-		r.stats = newAccumulator(ent.Value)
-	}
-	r.stats.Write(ent)
-	return ent, nil
-}
-
-// Close finalizes the Reader
-func (r *Accumulator) Close() error {
-	r.stats.Close()
-	return r.r.Close()
-}
-
-// accumulator is the common internal inferface for creating a stat
-// this package defines at least one accumulator for all values qri works with
-// accumulators are one-way state machines that update with each Write
-type accumulator interface {
-	Stat
-	Write(ent dsio.Entry)
-	Close()
-}
-
-func newAccumulator(val interface{}) accumulator {
-	switch val.(type) {
-	default:
-		return &nullAcc{}
-	case float64, float32:
-		return newNumericAcc("number")
-	case int, int32, int64:
-		return newNumericAcc("integer")
-	case string:
-		return newStringAcc()
-	case bool:
-		return &boolAcc{}
-	case map[string]interface{}:
-		return &objectAcc{children: map[string]accumulator{}}
-	case []interface{}:
-		return &arrayAcc{}
-	}
-}
-
-type objectAcc struct {
-	children map[string]accumulator
-}
-
-var (
-	_ accumulator = (*objectAcc)(nil)
-	_ Statser     = (*objectAcc)(nil)
-)
-
-// Stats gets child stats of the accumulator as a Stat slice
-func (acc *objectAcc) Stats() (stats []Stat) {
-	stats = make([]Stat, len(acc.children))
-	keys := make([]string, len(acc.children))
-	i := 0
-	for key := range acc.children {
-		keys[i] = key
-		i++
-	}
-	sort.StringSlice(keys).Sort()
-	for j, key := range keys {
-		stats[j] = keyedStat{Stat: acc.children[key], key: key}
-	}
-	return stats
-}
-
-// Type indicates this stat accumulator kind
-func (acc *objectAcc) Type() string { return "object" }
-
-// Write adds an entry to the stat accumulator
-func (acc *objectAcc) Write(e dsio.Entry) {
-	if mapEntry, ok := e.Value.(map[string]interface{}); ok {
-		for key, val := range mapEntry {
-			if _, ok := acc.children[key]; !ok {
-				acc.children[key] = newAccumulator(val)
-			}
-			acc.children[key].Write(dsio.Entry{Key: key, Value: val})
-		}
-	}
-}
-
-// Map formats stat values as a map
-func (acc *objectAcc) Map() map[string]interface{} {
-	vals := map[string]interface{}{}
-	for key, val := range acc.children {
-		vals[key] = val.Map()
-	}
-	return vals
-}
-
-// Close finalizes the accumulator
-func (acc *objectAcc) Close() {
-	for _, val := range acc.children {
-		val.Close()
-	}
-}
-
-type arrayAcc struct {
-	children []accumulator
-}
-
-var (
-	_ accumulator = (*arrayAcc)(nil)
-	_ Statser     = (*arrayAcc)(nil)
-)
-
-// Stats gets child stats of the array accumulator
-func (acc *arrayAcc) Stats() (stats []Stat) {
-	stats = make([]Stat, len(acc.children))
-	for i, ch := range acc.children {
-		stats[i] = ch
-	}
-	return stats
-}
-
-// Type indicates this stat accumulator kind
-func (acc *arrayAcc) Type() string { return "array" }
-
-// Write adds an entry to the stat accumulator
-func (acc *arrayAcc) Write(e dsio.Entry) {
-	if arrayEntry, ok := e.Value.([]interface{}); ok {
-		for i, val := range arrayEntry {
-			if len(acc.children) == i {
-				acc.children = append(acc.children, newAccumulator(val))
-			}
-			acc.children[i].Write(dsio.Entry{Index: i, Value: val})
-		}
-	}
-}
-
-// Map formats stat values as a map
-func (acc *arrayAcc) Map() map[string]interface{} {
-	vals := make([]map[string]interface{}, len(acc.children))
-	for i, val := range acc.children {
-		vals[i] = val.Map()
-	}
-	// TODO (b5) -  this is silly
-	return map[string]interface{}{"values": vals}
-}
-
-// Close finalizes the accumulator
-func (acc *arrayAcc) Close() {
-	for _, val := range acc.children {
-		val.Close()
-	}
-}
-
-const (
-	maxUint  = ^uint(0)
-	maxFloat = float64(maxUint >> 1)
-	maxInt   = int(maxUint >> 1)
-	minInt   = -maxInt - 1
-)
-
-type numericAcc struct {
-	typ             string
-	count           int
-	min             float64
-	max             float64
-	mean            float64
-	median          float64
-	dividers        []float64
-	histogram       []float64
-	histogramSketch *histosketch.Sketch
-}
-
-var _ accumulator = (*numericAcc)(nil)
-
-func newNumericAcc(typ string) *numericAcc {
-	return &numericAcc{
-		typ:    typ,
-		max:    float64(minInt),
-		min:    float64(maxInt),
-		median: maxFloat,
-		// use histogram to accumulate values
-		histogram: []float64{},
-		// sketch implementation for approximate histogram calculation
-		histogramSketch: histosketch.New(HistogramCentroidCount),
-	}
-}
-
-// Type indicates this stat accumulator kind
-func (acc *numericAcc) Type() string { return "numeric" }
-
-// Write adds an entry to the stat accumulator
-func (acc *numericAcc) Write(e dsio.Entry) {
-	var v float64
-	switch x := e.Value.(type) {
-	case int:
-		v = float64(x)
-	case int32:
-		v = float64(x)
-	case int64:
-		v = float64(x)
-	case float32:
-		v = float64(x)
-	case float64:
-		v = x
-	default:
-		return
-	}
-
-	acc.histogramSketch.Add(v)
-
-	acc.mean += v
-	acc.count++
-	if v > acc.max {
-		acc.max = v
-	}
-	if v < acc.min {
-		acc.min = v
-	}
-}
-
-// Map formats stat values as a map
-func (acc *numericAcc) Map() map[string]interface{} {
-	if acc.count == 0 {
-		// avoid reporting default max/min figures, if count is above 0
-		// at least one entry has been checked
-		return map[string]interface{}{"count": 0}
-	}
-	m := map[string]interface{}{
-		"mean":  acc.mean,
-		"count": acc.count,
-		"min":   acc.min,
-		"max":   acc.max,
-	}
-
-	if acc.median != maxFloat {
-		m["median"] = acc.median
-	}
-
-	if acc.histogram != nil {
-		m["histogram"] = map[string][]float64{
-			"bins":        acc.dividers,
-			"frequencies": acc.histogram,
-		}
-	}
-
-	return m
-}
-
-// Close finalizes the accumulator
-func (acc *numericAcc) Close() {
-	// finalize avg
-	acc.mean = acc.mean / float64(acc.count)
-	acc.dividers, acc.histogram = acc.histogramSketch.Read()
-	acc.median = acc.histogramSketch.Median()
-}
-
-type stringAcc struct {
-	count       int
-	minLength   int
-	maxLength   int
-	unique      int
-	frequencies map[string]int
-	hll         *hyperloglog.Sketch
-	topk        *topk.Stream
-}
-
-var _ accumulator = (*stringAcc)(nil)
-
-func newStringAcc() *stringAcc {
-	return &stringAcc{
-		maxLength:   minInt,
-		minLength:   maxInt,
-		frequencies: map[string]int{},
-		hll:         hyperloglog.New16(),
-		topk:        topk.New(StopFreqCountThreshold),
-	}
-}
-
-// Type indicates this stat accumulator kind
-func (acc *stringAcc) Type() string { return "string" }
-
-// Write adds an entry to the stat accumulator
-func (acc *stringAcc) Write(e dsio.Entry) {
-	if str, ok := e.Value.(string); ok {
-		acc.count++
-
-		acc.topk.Insert(str, 1)
-		acc.hll.Insert([]byte(str))
-
-		if len(str) < acc.minLength {
-			acc.minLength = len(str)
-		}
-		if len(str) > acc.maxLength {
-			acc.maxLength = len(str)
-		}
-	}
-}
-
-// Map formats stat values as a map
-func (acc *stringAcc) Map() map[string]interface{} {
-	if acc.count == 0 {
-		// avoid reporting default max/min figures, if count is above 0
-		// at least one entry has been checked
-		return map[string]interface{}{"count": 0}
-	}
-
-	m := map[string]interface{}{
-		"count":     acc.count,
-		"minLength": acc.minLength,
-		"maxLength": acc.maxLength,
-	}
-
-	if acc.unique != 0 {
-		m["unique"] = acc.unique
-	}
-	if acc.frequencies != nil {
-		m["frequencies"] = acc.frequencies
-	}
-
-	return m
-}
-
-// Close finalizes the accumulator
-func (acc *stringAcc) Close() {
-	acc.frequencies = map[string]int{}
-	top := acc.topk.Keys()
-	for _, k := range top {
-		acc.frequencies[k.Key] = k.Count
-	}
-	acc.unique = int(acc.hll.Estimate())
-}
-
-type boolAcc struct {
-	count      int
-	trueCount  int
-	falseCount int
-}
-
-var _ accumulator = (*boolAcc)(nil)
-
-// Type indicates this stat accumulator kind
-func (acc *boolAcc) Type() string { return "boolean" }
-
-// Write adds an entry to the stat accumulator
-func (acc *boolAcc) Write(e dsio.Entry) {
-	if b, ok := e.Value.(bool); ok {
-		acc.count++
-		if b {
-			acc.trueCount++
-		} else {
-			acc.falseCount++
-		}
-	}
-}
-
-// Map formats stat values as a map
-func (acc *boolAcc) Map() map[string]interface{} {
-	return map[string]interface{}{
-		"count":      acc.count,
-		"trueCount":  acc.trueCount,
-		"falseCount": acc.falseCount,
-	}
-}
-
-// Close finalizes the accumulator
-func (acc *boolAcc) Close() {}
-
-type nullAcc struct {
-	count int
-}
-
-var _ accumulator = (*nullAcc)(nil)
-
-// Type indicates this stat accumulator kind
-func (acc *nullAcc) Type() string { return "null" }
-
-// Write adds an entry to the stat accumulator
-func (acc *nullAcc) Write(e dsio.Entry) {
-	if e.Value == nil {
-		acc.count++
-	}
-}
-
-// Map formats stat values as a map
-func (acc *nullAcc) Map() map[string]interface{} {
-	return map[string]interface{}{"count": acc.count}
-}
-
-// Close finalizes the accumulator
-func (acc *nullAcc) Close() {}
-
-type keyedStat struct {
-	Stat
-	key string
-}
-
-// Map returns the stat, adding the "key" key indicating which key in the target
-// array the stat belongs to
-func (ks keyedStat) Map() map[string]interface{} {
-	v := ks.Stat.Map()
-	v["key"] = ks.key
-	return v
+// FileInfoCacheKey combines modTime and filepath to create a cache key string
+func FileInfoCacheKey(fi os.FileInfo) string {
+	return fmt.Sprintf("%d-%s", fi.ModTime(), fi.Name())
 }
