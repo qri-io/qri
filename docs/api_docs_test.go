456bfe20e01e06c41d8d8c1e6d065cd5f2d11909
b5 <sparkle_pony_2000@qri.io>
2021-04-20 09:51:59 -0400

    docs(api): load all lib ".go" files into AST parser

--- docs/api_docs_test.go
+++ docs/api_docs_test.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/getkin/kin-openapi/openapi3"
 )
 
 func TestOpenAPIYAML(t *testing.T) {
-	buf := OpenAPIYAML()
+	buf, err := OpenAPIYAML()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(buf.String())
 
 	sw, err := openapi3.NewSwaggerLoader().LoadSwaggerFromData(buf.Bytes())
 	if err != nil {
