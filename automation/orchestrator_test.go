499a66c5864a33d42d73c6067ca4ccf548474579
ramfox <kasey@qri.io>
2021-05-18 20:20:24 -0400

    feat(automation): `orchestrator` struct and constructor
    Adds an `Orchestrator`, a system for managing automation on Qri.
    Currenly the orchestrator has a bare-bones `workflow.Store`, takes a
    `SaveFactory` and `ApplyFactory`, that direct the orchestrator
    on what to run during `RunWorkflow` and `DryRunWorkflow`.
    
    The bare-bones workflow store can only `s.Create` and `s.Get`.
    
    An integration test is up that covers constructing a new orchestrator,
    creating a workflow, getting a workflow, running a workflow, and dry
    running a workflow.

---
+++ automation/orchestrator_test.go
@@ -0,0 +1,96 @@
+package automation
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/qri-io/ioes"
+	"github.com/qri-io/qri/automation/workflow"
+	"github.com/qri-io/qri/event"
+)
+
+func TestIntegration(t *testing.T) {
+	ctx := context.Background()
+	ran := make(chan string)
+	runFuncFactory := func(ctx context.Context) Run {
+		return func(ctx context.Context, streams ioes.IOStreams, w *workflow.Workflow, runID string) error {
+			ran <- "ran!"
+			return nil
+		}
+	}
+	applied := make(chan string)
+	applyFuncFactory := func(ctx context.Context) Apply {
+		return func(ctx context.Context, streams ioes.IOStreams, w *workflow.Workflow) error {
+			applied <- "applied"
+			return nil
+		}
+	}
+
+	bus := event.NewBus(ctx)
+
+	store := workflow.NewMemStore()
+	opts := OrchestratorOptions{
+		WorkflowStore: store,
+	}
+	o, err := NewOrchestrator(ctx, bus, runFuncFactory, applyFuncFactory, opts)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer o.Shutdown()
+
+	expected := &workflow.Workflow{
+		DatasetID: "dataset_id",
+		OwnerID:   "profile_id",
+	}
+
+	got, err := o.CreateWorkflow("dataset_id", "profile_id")
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected.ID = got.ID
+	if diff := cmp.Diff(expected, got); diff != "" {
+		t.Errorf("workflow mismatch (-want +got):\n%s", diff)
+	}
+
+	got, err = o.GetWorkflow(expected.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if diff := cmp.Diff(expected, got); diff != "" {
+		t.Errorf("workflow mismatch (-want +got):\n%s", diff)
+	}
+
+	done := errOnTimeout(t, ran, "o.RunWorkflow error: timed out before run function called")
+	err = o.RunWorkflow(ctx, got.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	<-done
+
+	done = errOnTimeout(t, applied, "o.ApplyWorkflow error: timed out before apply function called")
+	err = o.ApplyWorkflow(ctx, got.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	<-done
+
+	done = errOnTimeout(t, ran, "o.handleTrigger error: time out before run function called")
+	bus.Publish(ctx, event.ETWorkflowTrigger, expected.ID)
+	<-done
+}
+
+func errOnTimeout(t *testing.T, c chan string, errMsg string) <-chan struct{} {
+	done := make(chan struct{})
+	go func() {
+		select {
+		case msg := <-c:
+			t.Log(msg)
+		case <-time.After(200 * time.Millisecond):
+			t.Errorf(errMsg)
+		}
+		done <- struct{}{}
+	}()
+	return done
+}
