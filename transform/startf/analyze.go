90400c85ca5b012d920a800b57dfd01568b82338
Dustin Long <me@dustmop.io>
2021-06-22 15:25:32 -0400

    Print the parsed AST

--- transform/startf/analyze.go
+++ transform/startf/analyze.go
@@ -1,12 +1,20 @@
 package startf
 
 import (
+	"encoding/json"
 	"fmt"
 	"go.starlark.net/starlark"
 	starlarkSyntax "go.starlark.net/syntax"
 )
 
 func analyzeScriptFile(thread *starlark.Thread, filename string) {
+	err := doAnalyze(thread, filename)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func doAnalyze(thread *starlark.Thread, filename string) error {
 	// ExecFile(thread *Thread, filename string, src interface{}, predeclared StringDict)
 	// SourceProgram(filename string, src interface{}, isPredeclared func(string) bool)
 	// f, err := syntax.Parse(filename string, src interface{}, 0 ?)
@@ -14,9 +22,17 @@ func analyzeScriptFile(thread *starlark.Thread, filename string) {
 
 	f, err := starlarkSyntax.Parse(filename, nil, 0)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	fmt.Printf("Parsed successfully!\n")
-	_ = f
+	data, err := json.MarshalIndent(f, "", " ")
+	if err != nil {
+		return err
+	}
+
+	text := string(data)
+	fmt.Printf("%s\n", text)
+
+	return nil
 }
