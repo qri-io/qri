fe12bd03d72553a871c8e0cc6b37f87ff9ed705e
b5 <sparkle_pony_2000@qri.io>
2021-06-23 21:32:04 -0400

    refactor(run.Store): move run event management from run.Store to Orchestrator
    Applying a lesson learned while working on the new collection subsystem: if we
    can avoid interfaces requiring an event bus or publisher, it's worth doing so.
    Move event handling logic up into the orchestrator where it only needs to be
    written once, and drop the requirements that run.Store implementations take a
    bus. This drops three methods from the run.Store interface
    
    Add an extension interface for optimized event writing, and check for that
    interface within orchestrator run ID event subscriptions. Move event persistence
    spec tests up into an orchestrator test.

--- automation/run/store_test.go
+++ automation/run/store_test.go
@@ -1,17 +1,13 @@
 package run_test
 
 import (
-	"context"
 	"testing"
 
 	"github.com/qri-io/qri/automation/run"
 	"github.com/qri-io/qri/automation/spec"
-	"github.com/qri-io/qri/event"
 )
 
 func TestMemStore(t *testing.T) {
-	ctx := context.Background()
-	bus := event.NewBus(ctx)
-	store := run.NewMemStore(bus)
+	store := run.NewMemStore()
 	spec.AssertRunStore(t, store)
 }
