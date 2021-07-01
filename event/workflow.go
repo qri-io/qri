4ab11cdced52893721e6ce6723642dffedf57d08
ramfox <kasey@qri.io>
2021-07-01 16:27:09 -0400

    refactor(event): add `WorkflowTriggerPayload`
    In order for the `Orchestrator` to know which `Trigger` invoked the `event.ETWorkflowTrigger` event, the event now expects a `event.WorkflowTriggerPayload`. The `event.WorkflowTriggerPayload` contains `OwnerID`, `WorkflowID`, and `TriggerID` fields.

--- event/workflow.go
+++ event/workflow.go
@@ -1,9 +1,17 @@
 package event
 
+import "github.com/qri-io/qri/profile"
+
 const (
 	// ETWorkflowTrigger signals that a workflow has been triggered
-	// Payload will be a string representation of the `workflow.ID`
-	// of the workflow that should be run
+	// Payload will be a WorkflowTriggerPayload
 	// This event should not block
 	ETWorkflowTrigger = Type("workflow:trigger")
 )
+
+// WorkflowTriggerPayload is the expected payload of the `ETWorkflowTrigger`
+type WorkflowTriggerPayload struct {
+	WorkflowID string
+	OwnerID    profile.ID
+	TriggerID  string
+}
