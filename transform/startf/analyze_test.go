78f8aa62fac02d4ba62096c22bebf2905fda3614
Dustin Long <me@dustmop.io>
2021-07-01 15:06:19 -0400

    Multiple control flow graph tests

--- transform/startf/analyze_test.go
+++ transform/startf/analyze_test.go
@@ -8,22 +8,7 @@ import (
 )
 
 func TestSimpleControlFlow(t *testing.T) {
-	filename := "testdata/simple.star"
-	f, err := syntax.Parse(filename, nil, 0)
-	if err != nil {
-		t.Fatal(err)
-	}
-	funcs, err := collectFunctionDefs(f.Stmts)
-	if err != nil {
-		t.Fatal(err)
-	}
-	callGraph := buildCallGraph(funcs)
-
-	fn := callGraph.lookup["use_branch"]
-	body := fn.fn.body
-
-	controlFlow := newControlFlow()
-	buildControlFlow(controlFlow, body)
+	controlFlow := makeControlFlowForFunction(t, "testdata/simple.star", "use_branch")
 
 	actual := controlFlow.stringify()
 	expect := `
@@ -44,4 +29,74 @@ func TestSimpleControlFlow(t *testing.T) {
 	if diff := cmp.Diff(expect, actual); diff != "" {
 		t.Errorf("result mismatch (-want +got):\n%s", diff)
 	}
+}
+
+func TestMultipleControlFlow(t *testing.T) {
+	controlFlow := makeControlFlowForFunction(t, "testdata/simple.star", "branch_multiple")
+
+	actual := controlFlow.stringify()
+	expect := `
+0: set! a = 1
+   set! b = 2
+  out: 1
+1: if [< a b]
+  out: 2,3
+2: set! c = [+ b 1]
+   set! d = a
+   set! e = [+ a b]
+  out: 4
+3: set! c = [+ a 1]
+   print(c)
+   set! e = [+ c 2]
+  out: 4
+4: print([% '%d' e])
+  out:
+`
+	actual = strings.Trim(actual, " \n")
+	expect = strings.Trim(expect, " \n")
+	if diff := cmp.Diff(expect, actual); diff != "" {
+		t.Errorf("result mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestNoElseControlFlow(t *testing.T) {
+	controlFlow := makeControlFlowForFunction(t, "testdata/simple.star", "branch_no_else")
+
+	actual := controlFlow.stringify()
+	expect := `
+0: set! a = 1
+   set! b = 2
+  out: 1
+1: if [< a b]
+  out: 2,3
+2: set! c = [+ b 1]
+   print([% '%d' c])
+  out: 3
+3: print([% '%d' b])
+  out:
+`
+	actual = strings.Trim(actual, " \n")
+	expect = strings.Trim(expect, " \n")
+	if diff := cmp.Diff(expect, actual); diff != "" {
+		t.Errorf("result mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func makeControlFlowForFunction(t *testing.T, filename, funcname string) *ControlFlow {
+	f, err := syntax.Parse(filename, nil, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	funcs, err := collectFunctionDefs(f.Stmts)
+	if err != nil {
+		t.Fatal(err)
+	}
+	callGraph := buildCallGraph(funcs)
+
+	fn := callGraph.lookup[funcname]
+	body := fn.fn.body
+
+	controlFlow := newControlFlow()
+	buildControlFlow(controlFlow, body)
+	return controlFlow
 }
--- transform/startf/testdata/simple.star
+++ transform/startf/testdata/simple.star
@@ -7,3 +7,42 @@ def use_branch():
   else:
     c = a + 1
   print('%d' % c)
+
+
+def branch_multiple():
+  a = 1
+  b = 2
+  if a < b:
+    c = b + 1
+    d = a
+    e = a + b
+  else:
+    c = a + 1
+    print(c)
+    e = c + 2
+  print('%d' % e)
+
+
+def branch_no_else():
+  a = 1
+  b = 2
+  if a < b:
+    c = b + 1
+    print('%d' % c)
+  print('%d' % b)
+
+
+def branch_nested():
+  a = 1
+  b = 2
+  if a < b:
+    c = b + 1
+    d = a
+    if d > c:
+      c = d + 2
+    e = c + 2
+  else:
+    c = a + 1
+    print(c)
+    e = c + 2
+  print('%d' % e)
