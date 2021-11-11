f93b22c6d1e75ea57703f2d21746b1304d857e40
Dustin Long <me@dustmop.io>
2021-11-11 09:09:07 -0500

    refactor(api): Remove old pagination from api responses

--- api/util/responses_test.go
+++ api/util/responses_test.go
@@ -1,58 +1,21 @@
 package util
 
 import (
-	"fmt"
-	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestWritePageResponse(t *testing.T) {
-	cases := []struct {
-		page   Page
-		expect string
-	}{
-		{
-			Page{Number: 1, Size: DefaultPageSize},
-			`{"data":"data","meta":{"code":200},"pagination":{"page":1,"pageSize":50,"nextUrl":"https://example.com?page=2","prevUrl":""}}`,
-		},
-		{
-			Page{Number: 1, Size: DefaultPageSize, ResultCount: 200},
-			`{"data":"data","meta":{"code":200},"pagination":{"page":1,"pageSize":50,"resultCount":200,"nextUrl":"https://example.com?page=2","prevUrl":""}}`,
-		},
-		{
-			Page{Number: 2, Size: DefaultPageSize, ResultCount: 100},
-			`{"data":"data","meta":{"code":200},"pagination":{"page":2,"pageSize":50,"resultCount":100,"nextUrl":"","prevUrl":"https://example.com?page=1"}}`,
-		},
-		{
-			Page{Number: 2, Size: DefaultPageSize, ResultCount: 200},
-			`{"data":"data","meta":{"code":200},"pagination":{"page":2,"pageSize":50,"resultCount":200,"nextUrl":"https://example.com?page=3","prevUrl":"https://example.com?page=1"}}`,
-		},
-		{
-			Page{Number: 2, Size: 5, ResultCount: 200},
-			`{"data":"data","meta":{"code":200},"pagination":{"page":2,"pageSize":5,"resultCount":200,"nextUrl":"https://example.com?page=3\u0026pageSize=5","prevUrl":"https://example.com?page=1\u0026pageSize=5"}}`,
-		},
+func TestWriteResponseWithNextPageJSON(t *testing.T) {
+	rr := httptest.NewRecorder()
+
+	if err := WriteResponseWithNextPageJSON(rr, "data", "/next", `{"start":5}`); err != nil {
+		t.Fatal(err)
 	}
 
-	for i, c := range cases {
-		t.Run(fmt.Sprintf("case_%d", i), func(t *testing.T) {
-			rr := httptest.NewRecorder()
+	actual := rr.Body.String()
+	expect := `{"data":"data","meta":{"code":200},"nextPage":{"method":"POST","url":"/next","contentType":"application/json","jsonBody":"{\"start\":5}"}}`
 
-			req, err := http.NewRequest("GET", "https://example.com", nil)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			if err := WritePageResponse(rr, "data", req, c.page); err != nil {
-				t.Fatal(err)
-			}
-
-			got := rr.Body.String()
-
-			if c.expect != got {
-				t.Errorf("result mismatch. expected:\n%s\ngot:\n%s", c.expect, got)
-			}
-
-		})
+	if expect != actual {
+		t.Errorf("result mismatch. expected:\n%s\ngot:\n%s", expect, actual)
 	}
 }
