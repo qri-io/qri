84246a91e8f4680adb64394c5f83da6ebc2e16c2
Dustin Long <me@dustmop.io>
2021-12-02 09:55:35 -0500

    docs(cursor): Describe Cursor and how its used for pagination.

--- lib/cursor.go
+++ lib/cursor.go
@@ -8,6 +8,30 @@ import (
 	"strings"
 )
 
+// Cursor represents a position into a list of paginated results. Users
+// of this interface can retrieve all results by repeatedly calling
+// `cursor.Next()` until it returns ErrCursorComplete. When this error is
+// returned, the other returned value may still have data; users should
+// append this to their total collection of results.
+//
+// The implementation of a Cursor contains info about the method being
+// called, as well as whatever input parameters that method requires.
+// Moving to the next page of results is done by adjusting the input
+// parameters as needed to retrieve the next page.
+//
+// Methods that want to create and return a Cursor are encouraged to use
+// the helper `scope.MakeCursor`. The arguments to this method are the
+// number of results being returned in the current page, as well as
+// the input parameters to the method, after being adjusted. This method
+// will correctly handle the invariant that no Cursor should be returned
+// if the returned list of items is empty.
+//
+// Finally, a Cursor can be serialized for use in HTTP requests by using
+// the ToParams method. This basically just converts the input params
+// into a map of key value pairs. If passed as a POST json body, the
+// receiving HTTP server will be able to convert them back into input
+// params to retrieve the next page of results.
+
 var (
 	// ErrCursorComplete is returned by Cursor.Next when the cursor is completed
 	ErrCursorComplete = errors.New("cursor is complete")
