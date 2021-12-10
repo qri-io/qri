cd5b6510b377f6bb6cb44a64a6505b314ae5a1c9
ramfox <kasey@qri.io>
2021-12-10 11:37:38 -0500

    feat(params): add `ListParams` interface
    The `ListParams` interface has one method `ListParamsFromRequest`, which will take a request & pull list params form the url query. If the receiver `params.List` already contains listing information, the method will error (we only want to allow users one way at a time to indicate listing params, either in the body of the request or in the url query)

--- base/params/list.go
+++ base/params/list.go
@@ -1,7 +1,22 @@
 // Package params defines generic input parameter structures
 package params
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+var (
+	// ErrListParamsNotEmpty indicates that there are list values in the params
+	ErrListParamsNotEmpty = fmt.Errorf("list params not empty")
+)
+
+type ListParams interface {
+	// ListParamsFromRequest pulls list params from the URL
+	ListParamsFromRequest(r *http.Request) (List, error)
+}
 
 // ListAll uses a limit of -1 & offset of 0 as a sentinel value for listing
 // all items
@@ -13,6 +28,43 @@ type List struct {
 	OrderBy OrderBy
 	Limit   int
 	Offset  int
+}
+
+// ListParamsFromRequest satisfies the ListParams interface
+func (lp List) ListParamsFromRequest(r *http.Request) (List, error) {
+	l := List{}
+	limitStr := r.URL.Query().Get("limit")
+	if limitStr != "" {
+		limit, err := strconv.ParseInt(limitStr, 10, 0)
+		if err != nil {
+			return lp, err
+		}
+		l.Limit = int(limit)
+	}
+	offsetStr := r.URL.Query().Get("offset")
+	if offsetStr != "" {
+		offset, err := strconv.ParseInt(offsetStr, 10, 0)
+		if err != nil {
+			return lp, err
+		}
+		l.Offset = int(offset)
+	}
+
+	filterStr := r.URL.Query().Get("filter")
+	if filterStr != "" {
+		l = l.WithFilters(strings.Split(r.FormValue("filter"), ",")...)
+	}
+
+	l = l.WithOrderBy(r.URL.Query().Get("orderby"))
+
+	if l.IsEmpty() {
+		return lp, nil
+	}
+	if !lp.IsEmpty() {
+		return lp, ErrListParamsNotEmpty
+	}
+
+	return l, nil
 }
 
 // All returns true if List is attempting to list all available items
@@ -29,6 +81,13 @@ func (lp List) Validate() error {
 		return fmt.Errorf("offset of %d is out of bounds", lp.Offset)
 	}
 	return nil
+}
+
+// IsEmpty determines if the List struct is empty
+func (lp List) IsEmpty() bool {
+	return lp.Limit == 0 && lp.Offset == 0 &&
+		(lp.Filter == nil || len(lp.Filter) == 0) &&
+		(lp.OrderBy == nil || len(lp.OrderBy) == 0)
 }
 
 // WithOffsetLimit returns a new List struct that replaces the offset & limit
--- base/params/list_test.go
+++ base/params/list_test.go
@@ -1,11 +1,83 @@
 package params
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
+
+func TestListParamsFromRequest(t *testing.T) {
+	// should work for nil list params & request with no list information
+	r, err := http.NewRequest("POST", "/test", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// should not error if there is an already populated params.List and no list
+	// information in the request
+	expect := List{Offset: 10}
+	got, err := List{Offset: 10}.ListParamsFromRequest(r)
+	if err != nil {
+		t.Fatalf("ListParamsFromRequest should not error if there is no list information in the request: %s", err)
+	}
+	if diff := cmp.Diff(expect, got); diff != "" {
+		t.Errorf("params.List mismatch (+want,-got):\n%s", diff)
+	}
+
+	// add list queries to the request
+	q := r.URL.Query()
+	q.Add("offset", "10")
+	q.Add("limit", "10")
+	q.Add("filter", "username:peer")
+	q.Add("orderby", "+name,-updated")
+	r.URL.RawQuery = q.Encode()
+
+	got = List{}
+	expect = List{
+		Offset:  10,
+		Limit:   10,
+		Filter:  []string{"username:peer"},
+		OrderBy: OrderBy{{Key: "name", Direction: OrderASC}, {Key: "updated", Direction: OrderDESC}},
+	}
+	got, err = got.ListParamsFromRequest(r)
+	if err != nil {
+		t.Fatalf("ListParamsFromRequest unexpected error: %s", err)
+	}
+	if diff := cmp.Diff(expect, got); diff != "" {
+		t.Errorf("params.List mismatch (+want,-got):\n%s", diff)
+	}
+
+	// should error if list params are not empty & there is list info in the request
+	got = List{Filter: []string{"name:test"}}
+	got, err = got.ListParamsFromRequest(r)
+	if !errors.Is(err, ErrListParamsNotEmpty) {
+		t.Errorf("expected error to be %q, got %s", ErrListParamsNotEmpty, err)
+	}
+}
+
+func TestIsEmpty(t *testing.T) {
+	cases := []struct {
+		description string
+		lp          List
+		expect      bool
+	}{
+		{"empty list params", List{}, true},
+		{"each field empty", List{Offset: 0, Limit: 0, Filter: []string{}, OrderBy: OrderBy{}}, true},
+		{"has offset", List{Offset: 1}, false},
+		{"has limit", List{Limit: 1}, false},
+		{"has filter", List{Filter: []string{"public:true"}}, false},
+		{"has orderby", List{OrderBy: OrderBy{{Key: "", Direction: ""}}}, false},
+	}
+	for _, c := range cases {
+		got := c.lp.IsEmpty()
+		if got != c.expect {
+			fmt.Errorf("error in case %q, expected %t, got %t", c.description, c.expect, got)
+		}
+	}
+}
 
 func TestParamsWith(t *testing.T) {
 	expect := List{
